3c953a9829553541cb6a02ad73557246754762fb
Michael Okarimia <git@michaelokarimia.com>
2012-08-03 10:57:11 +0100

    First unit test of scraper

---
+++ PageScraper/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+
+namespace PageScraper
+{
+    public class Class1
+    {
+        string url = "http://www.wegottickets.com/searchresults/page/1/all";
+
+       public void Scrape()
+
+       {
+           HtmlWeb htmlWeb = new HtmlWeb();
+
+           HtmlDocument doc = htmlWeb.Load(url);
+           Document = doc;
+       }
+
+        public HtmlDocument Document { get; set; }
+
+        public IList<String> GetAllEventNodes()
+        {
+            List<string> matchedNodes = new List<string>();
+            var content = Document.GetElementbyId("content");
+            var nodes =  content.Descendants();
+            foreach (HtmlNode node in nodes)
+            {
+                if (node.Attributes.Contains("class"))
+                    matchedNodes.Add(node.ToString());
+            }
+           // nodes.Attributes
+
+            return matchedNodes;
+        }
+    }
+}
---
+++ UnitTests/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PageScraper;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ScraperTests
+    {
+        Class1 scraper;
+
+        [SetUp]
+        public void Setup()
+        {
+            scraper = new Class1();
+            scraper.Scrape();
+
+        }
+
+        [Test]
+        public void ScraperCanGetDocumentWithValidUrl()
+        {
+            Assert.NotNull(scraper.Document);
+        }
+
+    [Test]
+        public void CanfindNode()
+    {
+        Assert.Equals(10, scraper.GetAllEventNodes().Count);
+    }
+    }
+}
